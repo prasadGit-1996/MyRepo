adc5142154276d38815599bae034b569b19dc287
Dev2 <Dev2@gmail.com>
2020-04-10 22:52:46 +0530

    main() modified by Dev1

--- DemoProj1/src/com/dp/test/Test.java
+++ DemoProj1/src/com/dp/test/Test.java
@@ -1,8 +1,11 @@
 package com.dp.test;
 
+import com.dp.beans.Utility;
+
 public class Test {
 	public static void main(String[] args) {
-		
+		Utility utly = new Utility();
+		System.out.println(utly.sysDate());
 	}
 
 }
